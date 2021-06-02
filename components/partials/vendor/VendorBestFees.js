c3bc20cbd72f5b3a5974a2ecccadf783f59c02c1
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-02 21:20:12 +0200

    Update VendorBestFees.js

--- components/partials/vendor/VendorBestFees.js
+++ components/partials/vendor/VendorBestFees.js
@@ -9,18 +9,18 @@ const VendorBestFrees = () => (
             </div>
             <div className="ps-section__content">
                 <h5>
-                    It doesn’t cost a thing to list up to 50 items a month, and
-                    you only pay after your stuff sells. It’s just a small
-                    percent of the money you earn
+                    It doesn’t cost a thing to list your products
+and get your own e-store. You only pay the transaction fees
+once a sale is made.
                 </h5>
                 <div className="ps-section__numbers">
                     <figure>
-                        <h3>$0</h3>
-                        <span>List Fee</span>
+                        <h3>3%</h3>
+                        <span>Bank Fee</span>
                     </figure>
                     <figure>
                         <h3>5%</h3>
-                        <span>Final Value Fee</span>
+                        <span>Transaction Fee</span>
                     </figure>
                 </div>
                 <div className="ps-section__desc">
@@ -30,38 +30,32 @@ const VendorBestFrees = () => (
                         </figcaption>
                         <ul>
                             <li>
-                                A worldwide community of more than 160 million
-                                shoppers.
+                             Access to online shoppers and consumers across South Africa.
                             </li>
                             <li>
-                                Shipping labels you can print at home, with big
-                                discounts on postage.
+                           An online store to list, sell and advertise your products.
+
                             </li>
                             <li>
-                                Seller protection and customer support to help
-                                you sell your stuff.
+                             Logistics support so you don’t have to worry about managing your
+deliveries.
                             </li>
                         </ul>
                     </figure>
                 </div>
                 <div className="ps-section__highlight">
-                    <img src="img/icons/vendor-4.png" alt="Suppl-i" />
+//                     <img src="img/icons/vendor-4.png" alt="Suppl-i" />
                     <figure>
                         <p>
-                            We process payments with PayPal, an external
-                            payments platform that allows you to process
-                            transactions with a variety of payment methods.
-                            Funds from PayPal sales on Suppl-i will be deposited
-                            into your PayPal account.
+                          We process payment with Peach Payments, an external
+payment platform that allows us to process a variety of payment
+methods. Funds paid to Suppl-i for your goods will be paid forward
+directly to your business account, less the transaction fees.
                         </p>
                     </figure>
                 </div>
                 <div className="ps-section__footer">
-                    <p>
-                        Listing fees are billed for 0.20 USD, so if your bank’s
-                        currency is not USD, the amount in your currency may
-                        vary based on changes in the exchange rate.
-                    </p>
+                
                 </div>
             </div>
         </div>
