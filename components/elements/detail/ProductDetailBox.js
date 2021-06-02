fcb82e832cbfe91e039038df99b2e790ba0eb85c
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-02 17:36:32 +0200

    Update ProductDetailBox.js

--- components/elements/detail/ProductDetailBox.js
+++ components/elements/detail/ProductDetailBox.js
@@ -7,7 +7,7 @@ import { sample, products } from '../../../public/static/data/product';
 import { getProduct } from '../../../actions/products';
 import Widgets from '../../partials/product/ProductWidgets';
 import React, { Component } from 'react';
-
+import { Spin } from 'antd';
 export class ProductDetailBox extends Component {
     componentDidMount() {
         this.props.getProduct(this.props.pid);
@@ -16,42 +16,65 @@ export class ProductDetailBox extends Component {
         console.log(this.props);
         const { newProduct } = this.props;
         return (
-            <div>
-                <div className="ps-product--detail ps-product--box">
-                    <div className="ps-product__header ps-product__box">
-                        <ThumbnailBox
-                            product={
-                                this.props.newProduct && this.props.newProduct
-                            }
-                        />
-                        {this.props.newProduct && (
-                            <InformationBox
+            <>
+                {this.props.productLoading && this.props.productLoading && (
+                    <div
+                        style={{
+                            borderRadius: ' 4px',
+                            textAlign: 'center',
+                            left: ' 20px',
+                            tbottom: '40px',
+                            marginBottom: '20px',
+                            padding: '30px 50px',
+                            background: '#fff',
+                            zIndex: '99',
+                            width: '100%',
+                            height: '100%',
+                            margin: 'auto',
+                            display: 'block',
+                            position: 'fixed',
+                        }}>
+                        <Spin size="large" />
+                    </div>
+                )}
+                <div>
+                    <div className="ps-product--detail ps-product--box">
+                        <div className="ps-product__header ps-product__box">
+                            <ThumbnailBox
                                 product={
                                     this.props.newProduct &&
                                     this.props.newProduct
                                 }
                             />
-                        )}
-                    </div>
-                    <div className="ps-product__content">
-                        <div className="row">
-                            <div className="col-xl-9">
-                                <DescriptionBox
+                            {this.props.newProduct && (
+                                <InformationBox
                                     product={
                                         this.props.newProduct &&
                                         this.props.newProduct
                                     }
                                 />
-                            </div>
-                            <div className="col-xl-3">
-                                <div className="ps-product__box">
-                                    <Widgets />
+                            )}
+                        </div>
+                        <div className="ps-product__content">
+                            <div className="row">
+                                <div className="col-xl-9">
+                                    <DescriptionBox
+                                        product={
+                                            this.props.newProduct &&
+                                            this.props.newProduct
+                                        }
+                                    />
+                                </div>
+                                <div className="col-xl-3">
+                                    <div className="ps-product__box">
+                                        <Widgets />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
@@ -61,6 +84,7 @@ const mapStateToProps = (state) => ({
     newArrivals: state.newArrivals.products,
     randomProducts: state.randomProducts.products,
     newProduct: state.newProduct.product,
+    productLoading: state.newProduct.loading,
 });
 
 export default connect(mapStateToProps, { getProduct })(ProductDetailBox);
