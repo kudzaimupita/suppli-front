0018115b5305c4ed5320eb3edd50db5b9857ff08
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-02 20:48:00 +0200

    Update HeaderMobile.js

--- components/shared/headers/HeaderMobile.js
+++ components/shared/headers/HeaderMobile.js
@@ -12,35 +12,23 @@ class HeaderMobile extends Component {
     render() {
         return (
             <header className="header header--mobile">
-              
-                <div className="navigation--mobile" style={{backgroundColor:'white'}}>
+                <div
+                    className="navigation--mobile"
+                    style={{ backgroundColor: 'white' }}>
                     <div className="navigation__left">
                         <Link href="/">
                             <a className="ps-logo">
-                            <img style={{maxWidth:'130px'}} src="/static/img/suppli.png" alt="Suppl-i" />
+                                <img
+                                    style={{ maxWidth: '130px' }}
+                                    src="/static/img/Suppli.png"
+                                    alt="Suppl-i"
+                                />
                             </a>
                         </Link>
                     </div>
                     <div className="navigation__right">
                         <MobileHeaderActions />
                     </div>
-                </div>
-                <div className="ps-search--mobile">
-                    <form
-                        className="ps-form--search-mobile"
-                        action="/"
-                        method="get">
-                        <div className="form-group--nest">
-                            <input
-                                className="form-control"
-                                type="text"
-                                placeholder="Search something..."
-                            />
-                            <button>
-                                <i className="icon-magnifier"></i>
-                            </button>
-                        </div>
-                    </form>
                 </div>
             </header>
         );
