afab31ada316f5629dc9ef0643d754615f90aaed
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-02 11:58:21 +0200

    Update ThumbnailBox.js

--- components/elements/detail/modules/thumbnail/ThumbnailBox.js
+++ components/elements/detail/modules/thumbnail/ThumbnailBox.js
@@ -99,7 +99,7 @@ class ThumbnailBox extends Component {
                                                     )
                                                 }>
                                                 <img
-                                                    src={`https://suppli-api.herokuapp.com/img/products/${
+                                                    src={`https://suppli-images.s3.af-south-1.amazonaws.com/${
                                                         image && image
                                                     }`}
                                                     alt="Suppl-i"
