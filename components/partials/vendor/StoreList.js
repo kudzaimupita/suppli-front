5ce068c48d6ed75f185d7d855f166dac3ff5dd44
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-03 23:59:18 +0200

    Update StoreList.js

--- components/partials/vendor/StoreList.js
+++ components/partials/vendor/StoreList.js
@@ -2,45 +2,73 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Link from 'next/link';
 import { getVendors } from './../../../actions/vendors';
-
+import { Spin } from 'antd';
 class FurnitureCategories extends Component {
     componentDidMount() {
         this.props.getVendors();
     }
     render() {
         return (
-            <div className="ps-home-categories ps-section--furniture">
-                <div className="container">
-                    <div className="ps-section__content">
-                        <div
-                            className="ps-section__header center"
-                            style={{ textAlign: 'center', marginTop: '30px' }}>
-                            <h3>Our stores</h3>
-                        </div>
-                        <div className="row">
-                            {this.props.vendors &&
-                                this.props.vendors.map((vendor) => (
-                                    <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
-                                        <div
-                                            className="ps-block--category"
-                                            style={{
-                                                backgroundColor: 'white',
-                                                borderRadius: '50px',
-                                            }}>
-                                            <Link href="/shop">
-                                                <a></a>
-                                            </Link>
-                                            <img
-                                                style={{ borderRadius: '50px' }}
-                                                src={`https://suppli-images.s3.af-south-1.amazonaws.com/${vendor.logo}`}
-                                                alt="Suppl-i"
-                                            />
-                                            <p>{vendor.name}</p>
+            <>
+                {this.props.vendorsLoading && this.props.vendorsLoading && (
+                    <div
+                        style={{
+                            borderRadius: ' 4px',
+                            textAlign: 'center',
+                            // left: ' 20px',
+                            // top: '60px',
+                            marginBottom: '20px',
+                            padding: '300px 100px',
+                            background: '#fff',
+                            zIndex: '99',
+                            width: '100%',
+                            height: '100%',
+                            margin: 'auto',
+                            display: 'block',
+                            position: 'fixed',
+                        }}>
+                        <Spin size="large" />
+                    </div>
+                )}
+                <div className="ps-home-categories ps-section--furniture">
+                    <div className="container">
+                        <div className="ps-section__content">
+                            <div
+                                className="ps-section__header center"
+                                style={{
+                                    textAlign: 'center',
+                                    marginTop: '30px',
+                                }}>
+                                <h3>Our stores</h3>
+                            </div>
+                            <div className="row">
+                                {this.props.vendors &&
+                                    this.props.vendors.map((vendor) => (
+                                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                                            <div
+                                                className="ps-block--category"
+                                                style={{
+                                                    backgroundColor: 'white',
+                                                    borderRadius: '50px',
+                                                }}>
+                                                <Link
+                                                    href="/vendor/[vid]"
+                                                    as={`/vendor/${vendor._id}`}>
+                                                    <a className="ps-product__title"></a>
+                                                </Link>
+                                                <img
+                                                    style={{
+                                                        borderRadius: '50px',
+                                                    }}
+                                                    src={`https://suppli-images.s3.af-south-1.amazonaws.com/${vendor.logo}`}
+                                                    alt="Suppl-i"
+                                                />
+                                                <p>{vendor.name}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                ))}
+                                    ))}
 
-                            {/* <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
+                                {/* <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-6 ">
                                 <div
                                     className="ps-block--category"
                                     style={{
@@ -57,10 +85,11 @@ class FurnitureCategories extends Component {
                                         style={{ fontSize: '30px' }}></i>
                                 </div>
                             </div> */}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
@@ -71,6 +100,7 @@ FurnitureCategories.defaultProps = {
 
 const mapStateToProps = (state) => ({
     vendors: state.allVendors.vendors,
+    vendorsLoading: state.allVendors.loading,
     // catergories: state.catergories.catergories,
 });
 
