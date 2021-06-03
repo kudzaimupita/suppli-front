87c00a9a9f10b4c0201d53c00d3986495158010d
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-06-03 23:53:30 +0200

    Update Blank.js

--- components/partials/page/Blank.js
+++ components/partials/page/Blank.js
@@ -4,45 +4,109 @@ const BlankContent = () => (
     <div className="ps-section--custom">
         <div className="container">
             <div className="ps-section__header">
-                <h1>Page Heading</h1>
+                <h1>Returns Policy</h1>
             </div>
             <div className="ps-section__content">
-                <h3>Page Content</h3>
+                <h4>1. Consumer protection: cancellation and refunds</h4>
+                <hr />
                 <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                    natoque penatibus et magnis dis parturient montes, nascetur
-                    ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                    eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate
+                    This and the following paragraph are not contractually part
+                    of this agreement. These are statements of your rights as a
+                    consumer as defined in the Electronic Communications and
+                    Transactions Act 2002and of the procedures with which all
+                    our Sellers have been asked to comply. We have no
+                    responsibility if they fail to do so. We are not party to
+                    your contract with the Seller. Please refer any question
+                    about a Product to the Seller.
                 </p>
                 <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium, totam rem
-                    aperiam, eaque ipsa quae ab illo inventore veritatis et
-                    quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                    enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                    aut fugit, sed quia consequuntur magni dolores eos qui
-                </p>
-                <h4>He quick, brown fox jumps over a lazy dog.</h4>
-                <p>
-                    One morning, when Gregor Samsa woke from troubled dreams, he
-                    found himself transformed in his bed into a horrible vermin.
-                    He lay on his armour-like back, and if he lifted his head a
-                    little he could see his brown belly, slightly domed and
-                    divided by arches into stiff sections. The
+                    1.1. As required by the law, details of Seller’s after-sales
+                    service and guarantees, if any, are given on their various
+                    websites terms and conditions or in catalogues.
                 </p>
                 <p>
-                    A wonderful serenity has taken possession of my entire soul,
-                    like these sweet mornings of spring which I enjoy with my
-                    whole heart. I am alone, and feel the charm of existence in
-                    this spot, which was created for the bliss of souls like
-                    mine. I am so happy, my dear friend, so absorbed in the
-                    exquisite sense of mere tranquil existence, that I neglect
-                    my talents. I should be incapable of drawing a single stroke
-                    at the present moment; and yet I feel that I never was a
-                    greater artist than now. When, while the lovely valley teems
-                    with
+                    1.2. Products are delivered within 24 hours on the day you
+                    place an order to purchase a Product.
+                </p>
+                <p>
+                    1.3. You may cancel your order up to 3 hours prior to
+                    delivery.
+                </p>
+                <p>
+                    1.4. You are responsible for the cost of returning a
+                    Product. The Seller has no obligation to refund to you, your
+                    cost of re-packing and returning a Product. If you would
+                    like to return a product due to the driver or shopper’s
+                    negligence, Suppl-i will collect and return the item on your
+                    behalf.
+                </p>
+                <p>1.5. The option to cancel your order is not available:</p>
+                <p>
+                    1.5.1 if you purchase sealed goods which relate to health or
+                    hygiene, and they become unsealed after delivery, or cannot
+                    be re-sold for some other reason;
+                </p>
+
+                <p>
+                    1.5.2 sealed audio or sealed video recordings or sealed
+                    computer software, if these become unsealed after delivery;
+                </p>
+                <p>
+                    1.5.3 if the goods become mixed inseparably (according to
+                    their nature) with other items after delivery.
+                </p>
+                <p>
+                    1.6. In the event of cancellation of an order by you in
+                    compliance with these terms, the Seller will refund any
+                    money due to you within 2-5 days.
+                </p>
+                <p>
+                    1.7. This paragraph does not affect your rights in the event
+                    that the Products are faulty.{' '}
+                </p>
+                <h4>2. Products returned</h4>
+
+                <hr />
+                <p>
+                    These provisions apply in the event that you return any
+                    Product to the Seller for any reason:
+                </p>
+                <p>
+                    2.1. You should examine Product on receipt to check for
+                    possible defects and to satisfy yourself that it complies
+                    with your order, is of merchantable quality and in safe,
+                    clean and usable condition.
+                </p>
+                <p>
+                    2.2. The Product must be returned to the Seller as soon as
+                    any defect is discovered but not later than 7 days for
+                    non-perishable items only. Fresh food items cannot be
+                    returned.
+                </p>
+
+                <p>
+                    2.3. Product should be returned in accordance with the
+                    procedure set out on the Seller’s website or otherwise in
+                    his terms and conditions.
+                </p>
+                <p>2.4. So far as possible, a Product should be returned:</p>
+                <p>
+                    2.4.1 with both Product and all packaging as far as possible
+                    in their original condition;
+                </p>
+                <p>2.4.2 securely wrapped;</p>
+
+                <p>2.4.3 including the Seller’s delivery slip;</p>
+                <p>2.4.4 at your risk and cost.</p>
+                <p>
+                    2.5. In returning a defective Product, please enclose with
+                    it a note clearly stating the fault and when it arises or
+                    arose.
+                </p>
+                <p>
+                    2.6. If the Seller agrees that the Product is defective, he
+                    will send a new item to you or refund the full cost you have
+                    paid.
                 </p>
             </div>
         </div>
