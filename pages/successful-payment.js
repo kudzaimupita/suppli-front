7b0f5a463eedfa334993a6eae05d534ac76b6fae
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-07-10 12:45:25 +0200

    Create successful-payment.js

---
+++ pages/successful-payment.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import FooterDefault from '../components/shared/footers/FooterMarketPlace2';
+
+
+// import VendorBestFees from '../components/partials/vendor/VendorBestFees';
+import Suc from '../components/partials/account/success';
+// import VendorFaqs from '../../components/partials/vendor/VendorFaqs';
+import HeaderDefault from '../components/shared/headers/HeaderMarketPlace';
+import HeaderMobile from '../components/shared/headers/HeaderMobile';
+import NavigationList from '../components/shared/navigation/NavigationList';
+
+const BecomeAVendorPage = () => {
+
+
+    return (
+        <div className="site-content">
+            <HeaderDefault />
+            <HeaderMobile />
+            <NavigationList />
+            <div
+                className="ps-page--single"
+                style={{ backgroundColor: '#eeeeee' }}>
+                {/* <BreadCrumb breacrumb={breadCrumb} /> */}
+
+                <Suc />
+
+            </div>
+            <FooterDefault />
+        </div>
+    );
+};
+
+export default BecomeAVendorPage;
