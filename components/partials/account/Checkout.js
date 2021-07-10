386499ce3053ffce8bd0611e934c0be1b9f7ef5a
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-07-10 12:28:18 +0200

    Update Checkout.js

--- components/partials/account/Checkout.js
+++ components/partials/account/Checkout.js
@@ -7,6 +7,14 @@ class Checkout extends Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = "https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=" + 'B63C8819FE0D3495FA877CBC494FAEDD.uat01-vm-tx02'
+        script.async = true;
+        document.body.appendChild(script)
+
+
+    }
 
     render() {
         const { amount, cartTotal, cartItems } = this.props;
@@ -14,8 +22,9 @@ class Checkout extends Component {
             <div className="ps-checkout ps-section--shopping">
                 <div className="container">
                     <div className="ps-section__header">
-                        <h1>Checkout Information</h1>
+                        <h1>Checkout </h1>
                     </div>
+                    {/* <form style={{ backgroundColor: 'blue' }} action="{shopperResultUrl}" className="paymentWidgets" data-brands="VISA MASTER AMEX"></form> */}
                     <div className="ps-section__content">
                         <FormCheckoutInformation
                             amount={amount}
