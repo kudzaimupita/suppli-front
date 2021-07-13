0d9b5dce02f4a82b7cc95b70e4ec53f0b3445c1a
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-07-13 10:29:04 +0200

    Update Login.js

--- components/partials/account/Login.js
+++ components/partials/account/Login.js
@@ -140,7 +140,7 @@ class Login extends Component {
                                 <div className="form-group">
                                     <div className="ps-checkbox">
 
-                                        <a href='/' >
+                                        <a href='/account/forgot-password' >
                                             Forgot your password?
                                         </a>
                                     </div>
