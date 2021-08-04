abcdaa42b799c75d7b24d379dea54325c02e5d0b
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2021-08-04 10:24:04 +0200

    Update calculate.js

--- components/partials/account/modules/calculate.js
+++ components/partials/account/modules/calculate.js
@@ -10,35 +10,75 @@ export const createCompleteBooking = async (pick, drop) => {
         storeName: null,
     };
 
+
+
     const daaaa = await axios.post("https://droppergroup.co.za/droppa/services/plugins/quotes", object, {
         headers: {
             // "Access-Control-Allow-Origin": "*",
             "accept": "application/json",
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer c6eb3ef2-f678-4a3b-9278-e7dcd61cc10d:60a504c02114cc007591f0a8',
+            'Authorization': 'Bearer 66de47e2-9193-44e5-9bd5-55bb87564339:60f8012b2114cc00724765b4',
         }
     })
 
     return daaaa.data.amount
-    console.log(daaaa.data.amount)
-
-    // 
-
-    // return await axios.post('http://88.99.94.84:8080/droppa/services/' +
-    //     "v1/create/complete/booking",
-    //     object,
-    //     {
-    //         headers: {
-    //             "Accept": "application/json",
-    //             'Content-Type': 'application/json',
-    //             'Authorization': 'Bearer c6eb3ef2-f678-4a3b-9278-e7dcd61cc10d:60a504c02114cc007591f0a8',
-    //         }
-    //     },
-    //     // body: JSON.stringify(object)
-    // ).then((data) => console.log(data)).catch((e) => console.log(e))
-
-
 }
 
 
+export const CreateExpressBooking = async () => {
+    const dropTime = 2021 + '-' + 12 + '-' + 17 + 'T' + 17 + ':' + 10 + ':' + 50;
 
+    const object = {
+        dropOff: {
+            firstName: "Hello",
+            lastName: "Hello 12",
+            phone: "0798344109",
+            email: "Hello@gmail.com"
+        },
+        pickUp: {
+
+            firstName: "Nicolas",
+            lastName: "Hello 122",
+            phone: "0798344109",
+            email: "Hello@gmail.com"
+        },
+        vehicleType: "DROPPA_EXPRESS",
+        pickUpAddress: "13 Esdoring Techno Park, Centurion, 0157",
+        dropOffAddress: "Amampondo Drive, Rooihuiskraal North, Centurion, 0157, South Africa",
+        date: dropTime,
+        price: 100,
+        labour: 1,
+        comment: "Nicolas Rasekgala",
+        canopy: false,
+        load: 1,
+        province: "GAUTENG",
+        serviceId: "60f8012b2114cc00724765b4",
+        transportMode: "ROAD",
+        type: "EXPRESS_COURIER",
+        isExpress: true,
+        itemMass: 2,
+        parcelDimensions: [
+            {
+                "parcel_length": 20,
+                "parcel_breadth": 30,
+                "parcel_height": 40,
+                "parcel_mass": 50
+            }
+        ],
+        fromSuburb: "Centurion",
+        toSuburb: "Highveld",
+        pickUpPCode: "0157",
+        dropOffPCode: "0169"
+    }
+
+
+    const daaaa = await axios.post("https://droppergroup.co.za/droppa/services/v1/create/express/booking", object, {
+        headers: {
+            "accept": "application/json",
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 66de47e2-9193-44e5-9bd5-55bb87564339:60f8012b2114cc00724765b4',
+        }
+    })
+    console.log(daaaa
+    )
+}
