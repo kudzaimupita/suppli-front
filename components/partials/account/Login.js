31d1e6b9fb69ed1e7f3efc37818f0c94655766c7
Kudzai.Mupita <kudzai.mupita@kaleidocode.co.za>
2021-08-14 10:49:29 +0200

    add tailwind

--- components/partials/account/Login.js
+++ components/partials/account/Login.js
@@ -3,9 +3,10 @@ import Link from 'next/link';
 import Router from 'next/router';
 // import { login } from '../../../store/auth/action';
 import { login } from '../../../actions/auth';
-
 import { Form, Input, notification } from 'antd';
 import { connect } from 'react-redux';
+import { LockClosedIcon } from '@heroicons/react/solid'
+import '../../tailwind.scss'
 
 class Login extends Component {
     state = {
@@ -137,15 +138,34 @@ class Login extends Component {
                                         )}
                                     </Form.Item>
                                 </div>
-                                <div className="form-group">
+                                <div className="mb-2">
+                                    {/* <div className="flex items-center">
+                                        <input
+                                            id="remember_me"
+                                            name="remember_me"
+                                            type="checkbox"
+                                            className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                        />
+                                        <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                                            Remember me
+                                        </label>
+                                    </div> */}
+
+                                    <div className="text-sm">
+                                        <a href="/account/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                </div>
+                                {/* <div className="">
                                     <div className="ps-checkbox">
 
                                         <a href='/account/forgot-password' >
                                             Forgot your password?
                                         </a>
                                     </div>
-                                </div>
-                                <div className="form-group submit">
+                                </div> */}
+                                {/* <div className="form-group submit">
                                     <button
                                         style={{
                                             backgroundColor: '#62c4b0',
@@ -155,7 +175,16 @@ class Login extends Component {
                                         className="ps-btn ps-btn--fullwidth">
                                         Login
                                     </button>
-                                </div>
+                                </div> */}
+                                <button
+                                    type="submit"
+                                    className="submit group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                        <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                    </span>
+                                    Login
+                                </button>
                             </div>
                             <div className="ps-form__footer">
                                 {/* <p>Connect with:</p> */}
